8e1c6b92936d1933d81df3c32fa27a8e8516f927
bernabarros <bernardobarros.work@gmail.com>
2026-05-04 21:27:49 +0100

    Add feedback Slime enemy TakeDamage

--- Assets/Script/Enemy/EnemyController.cs
+++ Assets/Script/Enemy/EnemyController.cs
@@ -11,9 +11,20 @@ namespace Dungeonlicious.Assets.Script
         public static Action<GameObject> OnEnemyDeath;
         private Rigidbody _rigidbody;
 
+        private Renderer _renderer;
+        [SerializeField] private float _flashDuration = 0.1f;
+
+        private Color _originalColor;
+        private Coroutine _flashCoroutine;
+
         private void Start()
         {
+            _renderer = GetComponent<Renderer>();
+
+            _originalColor = _renderer.material.color;
+            
             _rigidbody = GetComponent<Rigidbody>();
+
             switch (_enemyType)
             {
                 case EnemyType.Slime:
@@ -28,6 +39,13 @@ namespace Dungeonlicious.Assets.Script
         public void TakeDamage(int damage)
         {
             _enemy.TakeDamage(damage);
+            if (_flashCoroutine != null)
+            {
+                StopCoroutine(_flashCoroutine);
+            }
+
+            _flashCoroutine = StartCoroutine(FlashWhite());
+
             if (_enemy.Health <= 0)
             {
                 OnEnemyDeath?.Invoke(gameObject);
@@ -40,7 +58,16 @@ namespace Dungeonlicious.Assets.Script
             Vector3 direction = (targetPosition - transform.position).normalized;
             _rigidbody.MovePosition(transform.position + direction * _speed * Time.deltaTime);
         }
+        private System.Collections.IEnumerator FlashWhite()
+        {
+            _renderer.material.color = Color.white;
+
+            yield return new WaitForSeconds(_flashDuration);
+
+            _renderer.material.color = _originalColor;
+        }
     }
+
 
     enum EnemyType
     {
