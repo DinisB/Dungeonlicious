b0d3a748347541f716ebf330e83f4fb0d6142cb6
bernabarros <bernardobarros.work@gmail.com>
2026-06-01 12:21:32 +0100

    Add flash to slime damage

--- Assets/Script/Enemy/SlimeHealth.cs
+++ Assets/Script/Enemy/SlimeHealth.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class SlimeHealth : MonoBehaviour, IDamageable
@@ -7,8 +8,16 @@ public class SlimeHealth : MonoBehaviour, IDamageable
     private SlimeAI slimeAI;
     public Vector3 position => transform.position;
 
+    [SerializeField] private Renderer slimeRenderer;
+    [SerializeField] private Color hitColor = Color.white;
+    [SerializeField] private float flashDuration = 0.1f;
+
+    private Color originalColor;
+    private Coroutine flashCoroutine;
+
     private void Awake()
     {
+        originalColor = slimeRenderer.material.color;
         currentHealth = maxHealth;
         slimeAI = GetComponent<SlimeAI>();
     }
@@ -22,6 +31,11 @@ public class SlimeHealth : MonoBehaviour, IDamageable
     {
         currentHealth -= amount;
 
+        if (flashCoroutine != null)
+            StopCoroutine(flashCoroutine);
+
+        flashCoroutine = StartCoroutine(Flash());
+
         slimeAI.Stagger(damager.transform.position);
 
         if (currentHealth <= 0)
@@ -29,9 +43,21 @@ public class SlimeHealth : MonoBehaviour, IDamageable
             Die();
         }
     }
-    
+
     private void Die()
     {
         Destroy(gameObject);
+    }
+
+    private IEnumerator Flash()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            slimeRenderer.material.color = hitColor;
+            yield return new WaitForSeconds(0.05f);
+
+            slimeRenderer.material.color = originalColor;
+            yield return new WaitForSeconds(0.05f);
+        }
     }
 }
