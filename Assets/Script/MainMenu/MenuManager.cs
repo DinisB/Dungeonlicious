0978c68cce13d495a9d1f0d973caa4cda90d6b5a
DinisB <dgamecreator@gmail.com>
2026-06-13 13:39:59 +0100

    resolution changer and back

--- Assets/Script/MainMenu/MenuManager.cs
+++ Assets/Script/MainMenu/MenuManager.cs
@@ -3,11 +3,15 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using System;
 using Dungeonlicious.Assets.Script;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
     [SerializeField] private string _playSceneName;
     [SerializeField] private TMP_InputField seedText;
+    [SerializeField] private Image background;
+    [SerializeField] private Sprite rooster;
+    [SerializeField] private Dropdown drop;
     private SeedKeeper seedKeeper;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,6 +19,11 @@ public class MenuManager : MonoBehaviour
         if (SceneManager.GetActiveScene().name == "Main Menu" || SceneManager.GetActiveScene().name == "VictoryCutscene")
         {
             DestroyAllDontDestroyOnLoad();
+        }
+
+        if (drop != null)
+        {
+            drop.onValueChanged.AddListener(ChangeResolution);
         }
 
         Time.timeScale = 1f;
@@ -36,10 +45,33 @@ public class MenuManager : MonoBehaviour
         }
     }
 
+    public void ChangeResolution(int index)
+    {
+        switch (index)
+        {
+            case 0:
+                Screen.SetResolution(1920, 1080, true);
+                break;
+
+            case 1:
+                Screen.SetResolution(3840, 2160, true);
+                break;
+
+            case 2:
+                Screen.SetResolution(640, 480, true);
+                break;
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    public void ChangeBackground()
+    {
+        background.sprite = rooster;
     }
 
     public void Play()
